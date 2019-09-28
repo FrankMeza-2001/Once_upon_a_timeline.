9d9a8d53b573a6f258d3840aba4f08ced7c8eb38
roberoalexande2001@outlook.com <55026188+LicencieSan@users.noreply.github.com>
2019-09-27 19:20:03 -0500

    agregando imagen y forma a automart

--- main_app/views.py
+++ main_app/views.py
@@ -17,14 +17,14 @@ def index(request):
     return render(request ,'index.html', {'autos': autos})
 
 class Auto:
-    def __init__(self, nombre, modelo, precio, color):
+    def __init__(self, nombre, modelo, precio, color, img_url):
         self.nombre = nombre
         self.modelo = modelo
         self.precio = precio
         self.color = color
+        self.img_url = img_url
         
-
-autos = [ Auto("VW jetta", 2018, 145000, "rojo"),
-            Auto("Batimovil", 2012, 14000000, "negro mate"),
-            Auto("Tesla Model S", 2018, 12000000, "rojo" ),
-            Auto("Delorean", 1985, 0, "plata" )]
+autos = [ Auto("VW jetta", 2018, 145000, "rojo", "https://img.motoryracing.com/noticias/portada/23000/23731-n.jpg"),
+            Auto("Batimovil", 2012, 14000000, "negro mate", "https://img.motoryracing.com/noticias/portada/23000/23731-n.jpg"),
+            Auto("Tesla Model S", 2018, 12000000, "rojo","https://img.motoryracing.com/noticias/portada/23000/23731-n.jpg"),
+            Auto("Delorean", 1985, 0, "plata", "https://img.motoryracing.com/noticias/portada/23000/23731-n.jpg")]
